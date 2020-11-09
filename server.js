c0432818597c927ea41bb9bde6d666a503b24c67
Stacy Burris <stacy1burris@gmail.com>
2020-11-09 14:27:47 -0800

    heroku deploy

--- server.js
+++ server.js
@@ -3,6 +3,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (request, response) => {
+  response.send('cool website');
+})
+
 app.listen(PORT, () => {
   console.log(`server up: ${PORT}`);
 });
